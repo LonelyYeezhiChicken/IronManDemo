17c0fd61dc340033d298368763906a2f638c9196
duke <dukelin@va7c>
2021-09-03 01:42:47 +0800

    || 2021-09-03 || 鐵人賽 || 自我介紹 ||

--- CsharpDemo/Program.cs
+++ CsharpDemo/Program.cs
@@ -10,6 +10,14 @@ namespace CsharpDemo
     {
         static void Main(string[] args)
         {
+            //印出文字
+            Console.WriteLine("姓名 : 孤獨一隻雞");
+            Console.WriteLine("年齡 : 48歲");
+            Console.WriteLine("血型 : O型");
+            Console.WriteLine("星座 : 金牛座");
+            Console.WriteLine("自我介紹 : 我像是一隻你可有可無的小雞");
+            Console.ReadKey();
+            
         }
     }
 }
